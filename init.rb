bdfd2cb9ae5489de3fed1d41d6aab74e1faabb53
tonchis <lucas.tolchinsky@gmail.com>
2013-02-01 17:31:00 -0300

    Reverts 303d338.

--- init.rb
+++ init.rb
@@ -1,3 +1,13 @@
+require "cuba"
+require "cuba/render"
+require "rack/protection"
+require "securerandom"
+require "mysql"
+require "json"
+require "sequel"
+require "shield"
+require "logger"
+
 DB = Sequel.connect("mysql://root@localhost/procon_development")
 DB.loggers << Logger.new(STDOUT)
 
